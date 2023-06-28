e45bb9ae2795161f396ce1a9c566c1f303a6b18d
bouarichaKarim <bouaricha.karim@gmail.com>
2023-06-28 14:08:17 +0000

    Add 3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,8 @@
+afe_print_division(a, b):
+    try:
+        result = a / b
+    except:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return (result)
